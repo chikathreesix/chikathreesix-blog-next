748fa5e8859b6c595568814beec2e5605f28d692
Ryo Chikazawa <chikathreesix@gmail.com>
2023-07-01 22:07:49 +0900

    Add redirects

--- next.config.js
+++ next.config.js
@@ -17,7 +17,41 @@ const nextConfig = {
   eslint: {
     /// Set this to false if you want production builds to abort if there's lint errors
     ignoreDuringBuilds: process.env.VERCEL_ENV === "production"
-  }
+  },
+  async redirects() {
+    return [
+      {
+        source: '/burning-needs',
+        destination: '/post/burning-needs',
+        permanent: true,
+      },
+      {
+        source: '/autify-2019',
+        destination: '/post/autify-2019',
+        permanent: true,
+      },
+      {
+        source: '/why-we-bet-on-testing-automation-now',
+        destination: '/post/why-we-bet-on-testing-automation-now',
+        permanent: true,
+      },
+      {
+        source: '/autify-one-year-anniversary',
+        destination: '/post/autify-one-year-anniversary',
+        permanent: true,
+      },
+      {
+        source: '/autify-values',
+        destination: '/post/autify-values',
+        permanent: true,
+      },
+      {
+        source: '/aiming-for-no1-in-the-global-market',
+        destination: '/post/aiming-for-no1-in-the-global-market',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
